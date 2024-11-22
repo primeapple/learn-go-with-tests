05b0c428f974a70b353269e0d509c67feb8f0a92
primeapple <toni.mueller.web@mailbox.org>
2024-11-22 12:00:12 +0100

    Add exercise about concurrency

---
+++ check-websites/check_websites.go
@@ -0,0 +1,27 @@
+package concurrency
+
+type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+	resultChannel := make(chan result)
+
+	for _, url := range urls {
+		go func() {
+            // send statement
+			resultChannel <- result{url, wc(url)}
+		}()
+	}
+
+	for i := 0; i < len(urls); i++ {
+        // receive statement
+		r := <-resultChannel
+		results[r.string] = r.bool
+	}
+
+	return results
+}
